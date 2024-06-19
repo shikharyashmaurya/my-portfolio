548a13c74ee4135a4d18bcbeaf51d93bcce21c1a
Shikhar <mauryashikhar87@gmail.com>
2024-06-19 14:04:13 +0530

    change in view

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ ScrollReveal().reveal(".home-content h1,.about-img",{origin:"left"});
 ScrollReveal().reveal(".home-content p,.about-content",{origin:"right"});
 
 const typed = new Typed(".multiple-text",{
-    strings:["Data Scientist","Data Analyst","Genai Engineer"],
+    strings:["Data Scientist","Genai Engineer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
